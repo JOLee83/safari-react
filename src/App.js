9d1193896047dcb87b56f9336bea9af63829a89c
JOLee83 <lee.justin.oliver@gmail.com>
2018-11-04 19:00:46 -0500

    back end connected to front, minimal css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  state = {
+    SeenAnimals: []
+
+  }
+  componentDidMount() {
+    fetch("https://localhost:5001/api/SeenAnimals")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          SeenAnimals: json
+        })
+      })
+  }
+  countAnimals = () => {
+    let count = 0
+    for (let i = 0; i < this.state.SeenAnimals.length; i++) {
+      count = count + this.state.SeenAnimals[i].countOfTimesSeen
+    }
+    return count
+  }
+  countCertainAnimals = () => {
+    let count = 0
+    for (let i = 0; i < this.state.SeenAnimals.length; i++) {
+      let animal = this.state.SeenAnimals[i].species
+      if (animal === "Lion" || animal === "Zebra" || animal === "Elephant") {
+        count = count + this.state.SeenAnimals[i].countOfTimesSeen
+      }
+    }
+    return count
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <header>Safari Vacation</header>
+        <main>
+          <h1>All the Animals I saw</h1>
+          <div>
+            {this.state.SeenAnimals.map(Animals => {
+              return (
+                <section key={Animals.id}>
+                  <h2>Species: {Animals.species}</h2>
+                  <h2>Sightings: {Animals.countOfTimesSeen}</h2>
+                  <h2>Location of Last Sighting: {Animals.locationOfLastSeen}</h2>
+                </section>
+              )
+            })}
+          </div>
+          <h1>All Animals Seen In Field</h1>
+          <div>
+
+            {this.state.SeenAnimals.map(Animals => {
+              if (Animals.locationOfLastSeen === "Field") {
+                return (
+                  <section key={Animals.id}>
+                    <h2>Species: {Animals.species}</h2>
+                    <h2>Sightings: {Animals.countOfTimesSeen}</h2>
+                    <h2>Location of Last Sighting: {Animals.locationOfLastSeen}</h2>
+                  </section>
+                )
+              }
+            })}
+          </div>
+          <h1>All Animals Not Seen In Field</h1>
+          <div>
+
+            {this.state.SeenAnimals.map(Animals => {
+              if (Animals.locationOfLastSeen !== "Field") {
+                return (
+                  <section key={Animals.id}>
+                    <h2>Species: {Animals.species}</h2>
+                    <h2>Sightings: {Animals.countOfTimesSeen}</h2>
+                    <h2>Location of Last Sighting: {Animals.locationOfLastSeen}</h2>
+                  </section>
+                )
+              }
+            })}
+          </div>
+          <h1>Total Animal Sightings</h1>
+          <aside>
+            <h2>{this.countAnimals()}</h2>
+          </aside>
+          <h1>Total Sightings of Lions, Zebras, and Elephants</h1>
+          <aside>
+            <h2>{this.countCertainAnimals()}</h2>
+          </aside>
+        </main>
+      </>
     );
   }
 }
